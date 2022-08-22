4c5f922586e371d2c6e9a679fb67164e5294f6e9
5onchangwoo <scw9410@gmail.com>
2022-08-22 20:24:45 +0900

    week-menu-controller 추가

--- server/src/controllers/week-menu-controller.js
+++ server/src/controllers/week-menu-controller.js
@@ -1,5 +1,8 @@
+const HttpStatus = require('http-status');
+const WeekMenuService = require('../services/week-menu-service');
+
 
 
 exports.selectWeekMenuList = async (req, res, next) => {
-    
+    const results = await WeekMenuService.selectWeekMenuList();
 }
