3cdf8c693fd896d679571303a6add36c99da05c3
5onchangwoo <scw9410@gmail.com>
2022-08-22 21:16:34 +0900

    week-menu-query.selectMenuPlan 추가

---
+++ server/src/databases/week-menu-query.js
@@ -0,0 +1,12 @@
+
+exports.selectMenuPlanList = () => {
+    return `
+        SELECT
+               *
+          FROM TBL_DAYPLAN A
+          JOIN TBL_MENUPLAN B ON(A.DAYPLAN_CODE = B.DAYPLAN_CODE)
+          JOIN TBL_MENU C ON(B.MENU_CODE = C.MENU_CODE)
+         WHERE A.DATE = ?
+           AND A.AMPM = ?
+    `
+}
