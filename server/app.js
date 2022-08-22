5b5ffc4d530d5a95799acdc0f5b3caf6391eac1f
5onchangwoo <scw9410@gmail.com>
2022-08-22 20:17:26 +0900

    week-menu 그룹핑 추가

--- server/app.js
+++ server/app.js
@@ -8,14 +8,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-const MenuRouter = require('./src/routes/menu-route');
-app.use('/menus', MenuRouter);
-
-const PlanRouter = require('./src/routes/plan-route');
-app.use('/plans', PlanRouter)
-
-const {swaggerUi, specs} = require('./src/swagger/swagger-specs.js');
-app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(specs, {explorer: true}));
+const WeekMenu = require('');
+app.use('/week-menus', WeekMenu);
 
 const port = 3030;
 const host = "0.0.0.0";
