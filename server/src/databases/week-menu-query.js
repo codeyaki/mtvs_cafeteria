cbd15ed64c740f5701f01681e91cc3680662f5e5
5onchangwoo <scw9410@gmail.com>
2022-08-23 09:52:03 +0900

    week-menu-query.countWeekPlan 추가

--- server/src/databases/week-menu-query.js
+++ server/src/databases/week-menu-query.js
@@ -11,10 +11,14 @@ exports.selectMenuPlanList = () => {
     `
 }
 
+exports.countWeekPlan = () => {
+    return `SELECT COUNT(*) FROM TBL_WEEKPLAN`;
+}
+
 exports.selectWeekPlan = () => {
     return `
         SELECT
-               *
+               A.*
           FROM TBL_WEEKPLAN A
          ORDER BY A.WEEK_END DESC
          LIMIT ?
