c22c93210778571964fdf56c83423917f6e5ee63
5onchangwoo <scw9410@gmail.com>
2022-08-23 17:28:37 +0900

    오류 수정

--- server/src/dto/response/week-and-day-and-menu-dto.js
+++ server/src/dto/response/week-and-day-and-menu-dto.js
@@ -1,13 +1,10 @@
-const { dateFormat } = require("../../utility/date-format");
-const DayplanDTO = require("../response/dayplan-res-dto");
-const MenuDTO = require("../response/menu-res-dto");
 
 class WeekAndDayAndMenuDTO{
     weekplanCode;
     weekplanNotice;
     weekplanStart;
     weekplanEnd;
-    menuplan = 
+    menuplan
     
 }
 
