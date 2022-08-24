ec06fe302ba5803ce4b2d4f33451b48ab7d8fa51
5onchangwoo <scw9410@gmail.com>
2022-08-24 17:57:51 +0900

    Weekplan

--- client/src/pages/Weekplans.js
+++ client/src/pages/Weekplans.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { callGetWeekplanAPI } from '../apis/WeekplanAPICalls';
 function Weekplans(){
     const results = useSelector(state => state.weekplanReducer);
+    const weekplan = results.results;
     const dispatch = useDispatch();
     useEffect(
         ()=> { dispatch(callGetWeekplanAPI() ); }
@@ -13,6 +14,8 @@ function Weekplans(){
     return (
         <article>
             <h1>식당 주간 식단표</h1>
+            <h2>시작일 {weekplan?.weekStart}</h2>
+            <h2>종료일 {weekplan?.weekEnd}</h2>
         </article>
     );
 
