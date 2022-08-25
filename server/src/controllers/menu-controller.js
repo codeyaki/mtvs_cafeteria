ad909f0c639c16f270d3a6fc0686129e49aac9d1
5onchangwoo <scw9410@gmail.com>
2022-08-25 19:20:17 +0900

    nodejs 메뉴 조회 추가

--- server/src/controllers/menu-controller.js
+++ server/src/controllers/menu-controller.js
@@ -1,14 +1,29 @@
+const httpStatus = require('http-status');
 const MenuService = require('../services/menu-service.js');
 
 
-exports.selectMenuList = async (req, res, next) => {
+exports.selectMenuList = async (req, res) => {
+    console.log('hi');
+    const rawCount = 40;
     const offset = parseInt(req.query.offset || '0');
-    const limit = parseInt(req.query.limit || '20');
-    const requestEntity = {
+    const limit = parseInt(req.query.limit || rawCount);
+    const limitOffset = {
         offset: offset,
         limit: limit
     }
-    const results = await MenuService.selectWeekMenuList(requestEntity);
-    console.log(results);
+    
+    const host = req.headers.host;
+    const results = await MenuService.selectMenuList(limitOffset);
+    const isNext = (offset+rawCount < results.count-1) ? true : false;
+    const isPrevious = (offset-rawCount > 0) ? true : false;
+    const next = isNext ? 'http://' + host + '/menus' + '?offset=' + (offset+rawCount) + "&limit=" + limit : null;
+    const previous = isPrevious ? 'http://' + host + '/menus' + '?offset=' + (offset-rawCount) + "&limit=" + limit : null;
+    const resBody = {
+        count: results.count,
+        next: next,
+        previous: previous,
+        results: results.results
+    }
+    return res.status(httpStatus.OK).json(resBody);
 };
 
--- server/src/services/menu-service.js
+++ server/src/services/menu-service.js
@@ -1,8 +1,14 @@
+const MenuController = require('../repositories/menu-repo.js');
+const getConnection = require('../databases/connection');
+const MenuResDTO = require('../dto/response/menu-res-dto.js');
 
-exports.selectWeekMenuList = () => {
-    return new Promise((resolve, reject) => {
+exports.selectMenuList = async (limitOffset) => {
+    const connection = getConnection();
+    const count = await MenuController.countMenu(connection);
+    
+    const menuList = await MenuController.selectMenuList(connection, limitOffset);
 
-
-        
-    })
+    return ({count:count, results:menuList})
+    
+    
 }
