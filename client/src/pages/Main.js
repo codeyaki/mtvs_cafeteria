59d5d460f1b3f4806837194535e4a47ce3e96387
5onchangwoo <scw9410@gmail.com>
2022-08-28 17:58:55 +0900

    mainIMG 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -7,7 +7,7 @@ function Main(){
 
             <h2 align="center">위치: 메타버스 허브 5층</h2>
             <div className="mainImgBox">
-                <img alt="mainImg" style={{width:'100%', margin: '0.5vmax'}} src="http://www.metaversehub.kr/images/sub/addition_facil_img02.jpg"></img>
+                <img alt="mainImg" style={{width:'100%', margin: '0.5vmax'}} src="./mainIMG.jpg"></img>
 
             </div>
         </div>
