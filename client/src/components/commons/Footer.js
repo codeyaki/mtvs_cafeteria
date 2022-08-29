509b14547d196c5c8a0a066f9a18a1f51f6e86a3
5onchangwoo <scw9410@gmail.com>
2022-08-29 21:59:31 +0900

    마무리

--- client/src/components/commons/Footer.js
+++ client/src/components/commons/Footer.js
@@ -2,7 +2,7 @@
 function Footer(){
     return (
         <footer>
-            <p align="center">Copyrightⓒ2008 team-phoenix All rights reserved.</p>
+            <p align="center">Copyrightⓒ2022 team-phoenix All rights reserved.</p>
         </footer>
 
     );
--- client/src/components/commons/Navbar.js
+++ client/src/components/commons/Navbar.js
@@ -10,8 +10,8 @@ function Navbar(){
     return (
         <nav>
             <NavLink align='center' className="navATag" to="/" style={ ({ isActive }) => isActive? activeStyle: undefined }>메인</NavLink>
-            <NavLink align='center' className="navATag" to="/weekplans" style={ ({ isActive }) => isActive? activeStyle: undefined }>식당 주간 식단표</NavLink>
-            <NavLink align='center' className="navATag" to="/menus" style={ ({ isActive }) => isActive? activeStyle: undefined }>전체 메뉴 조회하기</NavLink>
+            <NavLink align='center' className="navATag" to="/weekplans" style={ ({ isActive }) => isActive? activeStyle: undefined }>주간 메뉴 조회</NavLink>
+            <NavLink align='center' className="navATag" to="/menus" style={ ({ isActive }) => isActive? activeStyle: undefined }>전체 메뉴 조회</NavLink>
         </nav>
     );
 }
